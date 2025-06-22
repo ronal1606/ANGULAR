54fde85df82dc527b6c231621ceff271132bfb0a
ronal1606 <ronalsanchez.estudiante@iser.edu.co>
2025-06-22 02:19:44 -0500

    ANGULAR

--- angularproyecto/src/app/app.ts
+++ angularproyecto/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmailFormComponent } from "./components/email-form/email-form";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmailFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ angularproyecto/src/app/components/email-form/email-form.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-form',
+  templateUrl: './email-form.component.html',
+  styleUrls: ['./email-form.component.css']
+})
+export class EmailFormComponent {
+  email: string = '';
+
+  enviar() {
+    alert('Email a enviar: ' + this.email);
+  }
+}
